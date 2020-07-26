3eb3c0088f5ec1bc317f46562d2cb3b9ad603748
goodnessm3 <cthulhu3125@gmail.com>
2020-07-26 20:23:07 +0100

    stream stdout to webpage using websocketd example

---
+++ main/for_stdout.py
@@ -0,0 +1,7 @@
+import time
+import sys
+
+for x in range(10):
+    print(x)
+    time.sleep(0.5)
+    sys.stdout.flush()
--- main/start.py
+++ main/start.py
@@ -1,4 +1,6 @@
-from flask import Blueprint, flash, g, redirect, render_template, request, session, url_for
+from flask import Blueprint, flash, g, redirect, render_template, request, session, url_for, jsonify
+import main.fxns
+from subprocess import run
 
 bp = Blueprint('lookup_page', __name__, url_prefix='')
 
@@ -7,3 +9,23 @@ bp = Blueprint('lookup_page', __name__, url_prefix='')
 def begin():
 
     return render_template('page1/page1.html')
+
+
+@bp.route('/ajax', methods=('GET',))
+def ajax_example():
+
+    return render_template('ajax/ajax.html')
+
+
+@bp.route("/stringfunc/<astr>", methods=("GET",))
+def do_string_func(astr):
+
+    return main.fxns.string_func(astr)
+
+
+@bp.route("/websockettest", methods=("GET",))
+def websockettest():
+
+    run(r'''websocketd\websocketd.exe --port=8080 py main\for_stdout.py''')
+
+    return render_template('websocketpage/ws.html')
